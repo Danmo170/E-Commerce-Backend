631cb8d6b1c3fd30e27a83c7cba885818ac6340a
Danmo170 <daniel.carrillo.dev@gmail.com>
2026-02-27 23:42:02 -0500

    feat: Add repositories for Cart, Order, Product, and User entities

---
+++ src/main/java/com/projects/ecommerce/repository/CartRepository.java
@@ -0,0 +1,7 @@
+package com.projects.ecommerce.repository;
+
+import com.projects.ecommerce.model.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+}
---
+++ src/main/java/com/projects/ecommerce/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.projects.ecommerce.repository;
+
+import com.projects.ecommerce.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
