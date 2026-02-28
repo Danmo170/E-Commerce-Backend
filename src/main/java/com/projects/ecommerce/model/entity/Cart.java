9970d70eb75077f550e47d5dec05206311bec4cf
Danmo170 <daniel.carrillo.dev@gmail.com>
2026-02-27 22:37:27 -0500

    feat: Add Cart and CartItem entities for shopping cart functionality

---
+++ src/main/java/com/projects/ecommerce/model/entity/Cart.java
@@ -0,0 +1,37 @@
+package com.projects.ecommerce.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+
+        createdAt = LocalDateTime.now();
+
+    }
+
+}
