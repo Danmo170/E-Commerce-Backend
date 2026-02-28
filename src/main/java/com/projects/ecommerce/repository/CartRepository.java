190c7e7fe92c6c74a560c1a3f7aa6539eaf0e422
Danmo170 <daniel.carrillo.dev@gmail.com>
2026-02-27 23:50:24 -0500

    feat: Add methods to repositories for Cart, CartItem, and Order entities to support user-specific queries

--- src/main/java/com/projects/ecommerce/repository/CartRepository.java
+++ src/main/java/com/projects/ecommerce/repository/CartRepository.java
@@ -1,7 +1,13 @@
 package com.projects.ecommerce.repository;
 
 import com.projects.ecommerce.model.entity.Cart;
+import com.projects.ecommerce.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Optional<Cart> findByUser(User user);
+
 }
