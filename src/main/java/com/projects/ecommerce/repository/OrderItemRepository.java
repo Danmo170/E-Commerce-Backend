9013b0a11b6468d5ebee367cb9e44bbfbce3cedf
Danmo170 <daniel.carrillo.dev@gmail.com>
2026-03-02 21:57:47 -0500

    feat: Refactor package structure for DTOs and entities to improve organization

--- src/main/java/com/projects/ecommerce/repository/OrderItemRepository.java
+++ src/main/java/com/projects/ecommerce/repository/OrderItemRepository.java
@@ -1,6 +1,6 @@
 package com.projects.ecommerce.repository;
 
-import OrderItem;
+import com.projects.ecommerce.model.entity.Order.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
