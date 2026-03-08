396fa597ebf7d21ce8769d1069c06866a2888acd
Danmo170 <daniel.carrillo.dev@gmail.com>
2026-03-07 20:02:46 -0500

    fix: Small import fix

--- src/main/java/com/projects/ecommerce/controller/OrderController.java
+++ src/main/java/com/projects/ecommerce/controller/OrderController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
